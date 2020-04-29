d12a509f5deb97d42ba4d473ce48d8f5cc8e5ccc
JuchiaLu <36222739+JuchiaLu@users.noreply.github.com>
2020-04-29 13:16:16 +0800

    添加wallabag支持

--- src/module/authorize.jsx
+++ src/module/authorize.jsx
@@ -28,6 +28,17 @@ export default class Auth extends React.Component {
             username: "",
             password: "",
         },
+
+      wallabag: {
+        username: "",
+        password: "",
+        client_id:"",
+        client_secret:"",
+        server:"",
+        // access_token:"",
+        // refresh_token:""
+      },
+
         weizhi: {
             username: "",
             password: "",
@@ -39,6 +50,7 @@ export default class Auth extends React.Component {
         linnk  : undefined,
         instapaper : undefined,
         jianguo: undefined,
+        wallabag: undefined,
         weizhi : undefined,
         notion : undefined,
         youdao : undefined,
@@ -46,7 +58,7 @@ export default class Auth extends React.Component {
 
     onChange( state, value, flag ) {
         let notify;
-        const { dropbox, pocket, instapaper, linnk, evernote, onenote, gdrive, jianguo, yuque, notion, youdao, weizhi } = exp,
+        const { dropbox, pocket, instapaper, linnk, evernote, onenote, gdrive, jianguo,wallabag, yuque, notion, youdao, weizhi } = exp,
             clear = ( id, name ) => {
                 Object.keys( storage.secret[id] ).forEach( item => storage.secret[id][item] = "" );
                 storage.Safe( ()=> {
@@ -58,11 +70,17 @@ export default class Auth extends React.Component {
                 notify && notify.complete();
                 Object.keys( data ).forEach( item => storage.secret[id][item] = data[item] );
                 id == "jianguo" && ( storage.secret[id]["access_token"] = { username: data.username, password: data.password });
+                id == "wallabag" && ( storage.secret[id]["access_token"] = {
+                  access_token : data.access_token,
+                  refresh_token : data.refresh_token,
+                  expireDateMs : data.expireDateMs
+                });
                 storage.Safe( () => {
                     new Notify().Render( `已成功授权 ${name} 。` );
                     id == "linnk"      && this.setState({ secret: storage.secret, linnk: false });
                     id == "instapaper" && this.setState({ secret: storage.secret, instapaper: false });
                     id == "jianguo"    && this.setState({ secret: storage.secret, jianguo: false });
+                    id == "wallabag"    && this.setState({ secret: storage.secret, wallabag: false });
                     id == "weizhi"     && this.setState({ secret: storage.secret, weizhi: false });
                     id == "notion"     && this.setState({ secret: storage.secret, notion: notion.blocks });
                     id == "youdao"     && this.setState({ secret: storage.secret, youdao: youdao.folders });
@@ -98,6 +116,11 @@ export default class Auth extends React.Component {
             return;
         }
 
+        if ( state == "wallabag" && !flag && !storage.secret.wallabag.username ) {
+          this.setState({ wallabag: !this.state.wallabag });
+          return;
+        }
+
         if ( state == "weizhi" && !flag && !storage.secret.weizhi.username ) {
             this.setState({ weizhi: !this.state.weizhi });
             return;
@@ -117,6 +140,17 @@ export default class Auth extends React.Component {
                 this.props.jianguo.username = "";
                 this.props.jianguo.password = "";
             }
+
+          if ( state == "wallabag" ) {
+            this.props.wallabag.username = "";
+            this.props.wallabag.password = "";
+            this.props.wallabag.client_id = "";
+            this.props.wallabag.client_secret = "";
+            this.props.wallabag.server = "";
+            //this.props.wallabag.access_token = "";
+            //this.props.wallabag.refresh_token = "";
+          }
+
             if ( state == "weizhi" ) {
                 this.props.weizhi.username = "";
                 this.props.weizhi.password = "";
@@ -267,6 +301,33 @@ export default class Auth extends React.Component {
                     } else success( "jianguo", "坚果云", { username: this.props.jianguo.username, password: this.props.jianguo.password } );
                 });
                 break;
+
+          case "wallabag":
+            wallabag.Auth(
+              this.props.wallabag.username ,
+              this.props.wallabag.password ,
+              this.props.wallabag.client_id,
+              this.props.wallabag.client_secret,
+              this.props.wallabag.server, (result, error) => {
+                if ( error ) {
+                  failed( "授权错误，请重新授权。", wallabag.id, wallabag.name );
+                } else {
+                  let nowDate = new Date(Date.now());
+                  success( "wallabag", "Wallabag", {
+                    username: this.props.wallabag.username,
+                    password: this.props.wallabag.password ,
+                    client_id: this.props.wallabag.client_id,
+                    client_secret: this.props.wallabag.client_secret,
+                    server: this.props.wallabag.server,
+                    access_token: result.access_token,
+                    refresh_token: result.refresh_token,
+                    expireDateMs : nowDate.setSeconds(nowDate.getSeconds() + result.expires_in)
+                  });
+                }
+            });
+            break;
+
+
             case "weizhi":
                 if ( location.hash.startsWith( "#labs?auth=" ) ) {
                     this.props.weizhi.username = storage.secret.weizhi.username;
@@ -302,6 +363,10 @@ export default class Auth extends React.Component {
 
     jianguoOnChange( state, value ) {
         this.props.jianguo[state] = value;
+    }
+
+    wallabagOnChange( state, value ) {
+        this.props.wallabag[state] = value;
     }
 
     weizhiOnChange( state, value ) {
@@ -385,10 +450,10 @@ export default class Auth extends React.Component {
                             label={ this.state.secret.pocket.access_token ? "已授权 Pocket，是否取消授权？" : "是否连接并授权 Pocket ？" }
                             onChange={ (s)=>this.onChange( "pocket", s ) } />
 
-                        { this.state.secret.pocket.access_token && 
+                        { this.state.secret.pocket.access_token &&
                         <div ref="pocket_tags" style={{ "width": "60%" }}>
                             <TextField
-                                placeholder="请填入 Pocket 标签，默认为 simpread 每个标签用小写, 分割。" 
+                                placeholder="请填入 Pocket 标签，默认为 simpread 每个标签用小写, 分割。"
                                 value={ this.state.secret.pocket.tags }
                                 onChange={ (evt)=>this.save( "pocket", evt.target.value ) }
                             />
@@ -399,16 +464,16 @@ export default class Auth extends React.Component {
                             label={ this.state.secret.instapaper.access_token ? "已授权 Instapaper，是否取消授权？" : "是否连接并授权 Instapaper ？" }
                             onChange={ (s)=>this.onChange( "instapaper", s ) } />
 
-                        { this.state.instapaper && 
+                        { this.state.instapaper &&
                         <div ref="instapaper">
                             <div style={{ "display": "flex", "flex-direction": "row" }}>
                                 <TextField
-                                    placeholder="请填入 Instapaper 用户名，简悦不会记录你的用户名。" 
+                                    placeholder="请填入 Instapaper 用户名，简悦不会记录你的用户名。"
                                     onChange={ (evt)=>this.instapaperOnChange( "username", evt.target.value ) }
                                 />
                                 <TextField
                                     password={true}
-                                    placeholder="请填入 Instapaper 密码，简悦不会记录你的密码。" 
+                                    placeholder="请填入 Instapaper 密码，简悦不会记录你的密码。"
                                     onChange={ (evt)=>this.instapaperOnChange( "password", evt.target.value ) }
                                 />
                             </div>
@@ -425,25 +490,25 @@ export default class Auth extends React.Component {
                             label={ this.state.secret.linnk.access_token ? "已授权 Linnk，是否取消授权？" : "是否连接并授权 Linnk ？" }
                             onChange={ (s)=>this.onChange( "linnk", s ) } />
 
-                        { this.state.secret.linnk.access_token && 
+                        { this.state.secret.linnk.access_token &&
                             <div style={{ "width": "60%" }}>
                                 <TextField
                                     value={ this.state.secret.linnk.group_name }
-                                    placeholder="请填入 Linnk 收藏夹名称，默认保存到 收件箱。" 
+                                    placeholder="请填入 Linnk 收藏夹名称，默认保存到 收件箱。"
                                     onChange={ (evt)=>this.save( "linnk", evt.target.value ) }
                                 />
                             </div> }
 
-                        { this.state.linnk && 
+                        { this.state.linnk &&
                         <div ref="linnk">
                             <div style={{ "display": "flex", "flex-direction": "row" }}>
                                 <TextField
-                                    placeholder="请填入 Linnk 用户名，简悦不会记录你的用户名。" 
+                                    placeholder="请填入 Linnk 用户名，简悦不会记录你的用户名。"
                                     onChange={ (evt)=>this.linnkOnChange( "username", evt.target.value ) }
                                 />
                                 <TextField
                                     password={true}
-                                    placeholder="请填入 Linnk 密码，简悦不会记录你的密码。" 
+                                    placeholder="请填入 Linnk 密码，简悦不会记录你的密码。"
                                     onChange={ (evt)=>this.linnkOnChange( "password", evt.target.value ) }
                                 />
                             </div>
@@ -481,16 +546,16 @@ export default class Auth extends React.Component {
                             label={ this.state.secret.jianguo && this.state.secret.jianguo.username != "" ? "已授权 坚果云，是否取消授权？" : "是否连接并授权 坚果云 ？" }
                             onChange={ (s)=>this.onChange( "jianguo", s ) } />
                         </div>
-                        { this.state.jianguo && 
+                        { this.state.jianguo &&
                         <div ref="jianguo">
                             <div style={{ "display": "flex", "flex-direction": "row" }}>
                                 <TextField
-                                    placeholder="请填入 坚果云的 WebDAV 用户名，简悦不会记录你的用户名。" 
+                                    placeholder="请填入 坚果云的 WebDAV 用户名，简悦不会记录你的用户名。"
                                     onChange={ (evt)=>this.jianguoOnChange( "username", evt.target.value ) }
                                 />
                                 <TextField
                                     password={true}
-                                    placeholder="请填入 坚果云的 WebDAV 密码，简悦不会记录你的密码。" 
+                                    placeholder="请填入 坚果云的 WebDAV 密码，简悦不会记录你的密码。"
                                     onChange={ (evt)=>this.jianguoOnChange( "password", evt.target.value ) }
                                 />
                             </div>
@@ -502,6 +567,48 @@ export default class Auth extends React.Component {
                                 onClick={ (s)=>this.onChange( "jianguo", s, "login" ) } />
 
                         </div> }
+
+              <div className="version-tips" data-version="1.1.3" data-hits="wallabag">
+                <Switch width="100%" checked={ this.state.secret.wallabag && this.state.secret.wallabag.username != "" && this.state.secret.wallabag.password ? true : false }
+                        thumbedColor="#3F51B5" trackedColor="#7986CB" waves="md-waves-effect"
+                        label={ this.state.secret.wallabag && this.state.secret.wallabag.username != "" ? "已授权 Wallabag，是否取消授权？" : "是否连接并授权 Wallabag ？" }
+                        onChange={ (s)=>this.onChange( "wallabag", s ) } />
+              </div>
+              { this.state.wallabag &&
+              <div ref="wallabag">
+                <div style={{ "display": "flex", "flex-direction": "row" }}>
+                  <TextField
+                    placeholder="用户名"
+                    onChange={ (evt)=>this.wallabagOnChange( "username", evt.target.value ) }
+                  />
+                  <TextField
+                    password={true}
+                    placeholder="密码"
+                    onChange={ (evt)=>this.wallabagOnChange( "password", evt.target.value ) }
+                  />
+                  <TextField
+                    placeholder="客户端ID"
+                    onChange={ (evt)=>this.wallabagOnChange( "client_id", evt.target.value ) }
+                  />
+                  <TextField
+                    password={true}
+                    placeholder="客户端密钥"
+                    onChange={ (evt)=>this.wallabagOnChange( "client_secret", evt.target.value ) }
+                  />
+                  <TextField
+                    placeholder="服务器地址"
+                    onChange={ (evt)=>this.wallabagOnChange( "server", evt.target.value ) }
+                  />
+
+                </div>
+
+                <Button type="raised" width="100%" style={{ "margin": "0" }}
+                        text="绑定 Wallabag 的信息"
+                        color="#fff" backgroundColor="#3F51B5"
+                        waves="md-waves-effect md-waves-button"
+                        onClick={ (s)=>this.onChange( "wallabag", s, "login" ) } />
+
+              </div> }
 
                         <div className="version-tips" data-version="1.1.3" data-hits="yuque">
                         <Switch width="100%" checked={ this.state.secret.yuque.access_token != "" ? true : false }
@@ -516,7 +623,7 @@ export default class Auth extends React.Component {
                             label={ this.state.secret.notion.access_token ? "已授权 Notion，是否取消授权？" : "是否连接并授权 Notion ？" }
                             onChange={ (s)=>this.onChange( "notion", s ) } />
 
-                        { this.state.secret.notion.access_token && 
+                        { this.state.secret.notion.access_token &&
                             <div style={{ display: "flex","flex-direction": "row", "justify-content": "center" }}>
                             { this.state.notion ? <Dropdown name={ "请选择保存的位置，默认为第一个" } items={ this.state.notion } width="100%" onChange={ (v,n)=>this.save( "notion", v ) } />
                             : <Button type="flat" width="100%" style={{ "margin": "0" }}
@@ -532,7 +639,7 @@ export default class Auth extends React.Component {
                             label={ this.state.secret.youdao.access_token ? "已授权 有道云笔记" : "是否连接并授权 有道云笔记 ？" }
                             onChange={ (s)=>this.onChange( "youdao", s ) } />
 
-                        { this.state.secret.youdao.access_token && 
+                        { this.state.secret.youdao.access_token &&
                             <div style={{ display: "flex","flex-direction": "row", "justify-content": "center" }}>
                             { this.state.youdao ? <Dropdown name={ "请选择保存的位置，默认为第一个" } items={ this.state.youdao } width="100%" onChange={ (v,n)=>this.save( "youdao", v ) } />
                             : <Button type="flat" width="100%" style={{ "margin": "0" }}
@@ -549,16 +656,16 @@ export default class Auth extends React.Component {
                             label={ this.state.secret.weizhi && this.state.secret.weizhi.username != "" ? "已授权 为知笔记，是否取消授权？" : "是否连接并授权 为知笔记 ？" }
                             onChange={ (s)=>this.onChange( "weizhi", s ) } />
                         </div>
-                        { this.state.weizhi && 
+                        { this.state.weizhi &&
                         <div ref="weizhi">
                             <div style={{ "display": "flex", "flex-direction": "row" }}>
                                 <TextField
-                                    placeholder="请填入 为知笔记 的登录邮箱，简悦不会记录你的邮箱。" 
+                                    placeholder="请填入 为知笔记 的登录邮箱，简悦不会记录你的邮箱。"
                                     onChange={ (evt)=>this.weizhiOnChange( "username", evt.target.value ) }
                                 />
                                 <TextField
                                     password={true}
-                                    placeholder="请填入 为知笔记 的密码，简悦不会记录你的密码。" 
+                                    placeholder="请填入 为知笔记 的密码，简悦不会记录你的密码。"
                                     onChange={ (evt)=>this.weizhiOnChange( "password", evt.target.value ) }
                                 />
                             </div>
@@ -570,13 +677,13 @@ export default class Auth extends React.Component {
                                 onClick={ (s)=>this.onChange( "weizhi", s, "login" ) } />
 
                         </div> }
-    
+
                         <div className="version-tips" data-version="1.1.4" data-hits="webdav">
                         <div className="label" style={{'margin-bottom':' -15px'}}>WebDAV</div>
                         <div className="sublabel">简悦支持任意 WebDAV 的服务，包括：Box · TeraCLOUD 等</div>
-                        <TextField 
+                        <TextField
                             multi={ true } rows={8}
-                            placeholder={ `每行一组，格式为：{ "name": "网盘的名称", "user": "用户名", "password": "密码", "url": "webdav 地址" }` } 
+                            placeholder={ `每行一组，格式为：{ "name": "网盘的名称", "user": "用户名", "password": "密码", "url": "webdav 地址" }` }
                             value={ ( this.state.secret.webdav||[] ).join( "\n" ) }
                             onChange={ (e)=>this.webdavOnChange(e) }
                         />
